c3667d43a302ff14ab0d5a222f2da321f4b03635
alejoiza783 <aiza4840@gmail.com>
2025-11-03 20:25:54 -0500

    agregar y eliminar y editar de mascotas

--- Aplicaciones/Mascotas/views.py
+++ Aplicaciones/Mascotas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mascota
 from Aplicaciones.Personas.models import Persona
 
@@ -37,3 +37,38 @@ def nueva_mascota(request):
         return redirect('inicio')
     
     return render(request, 'nuevo_mascota.html', {'personas': personas})
+
+
+
+def editar_mascota(request, id):
+    mascota = get_object_or_404(Mascota, id=id)
+    personas = Persona.objects.all()
+    
+    if request.method == 'POST':
+        mascota.nombre = request.POST['nombre']
+        mascota.especie = request.POST['especie']
+        mascota.raza = request.POST['raza']
+        mascota.edad = request.POST['edad']
+        mascota.sexo = request.POST['sexo']
+        mascota.descripcion = request.POST['descripcion']
+        mascota.fecha_rescate = request.POST['fecha_rescate']
+        mascota.adoptado = 'adoptado' in request.POST
+        
+        if request.POST['dueño'] != '':
+            dueño = Persona.objects.get(id=request.POST['dueño'])
+            mascota.dueño = dueño
+        else:
+            mascota.dueño = None
+            
+        mascota.save()
+        return redirect('inicio')
+    
+    return render(request, 'editar_mascota.html', {
+        'mascota': mascota,
+        'personas': personas
+    })
+
+def eliminar_mascota(request, id):
+    mascota = get_object_or_404(Mascota, id=id)
+    mascota.delete()
+    return redirect('inicio')
