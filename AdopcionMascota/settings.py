adc7b616168371e870b3a1308b583702925e60a4
SantiagoAguila <cristianaguilac08@gmail.com>
2025-11-03 19:18:31 -0500

    subiendo coneccion archivo media y static

--- AdopcionMascota/settings.py
+++ AdopcionMascota/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,8 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ( 
+    os.path.join(BASE_DIR, 'AdopcionMascota/static'), 
+) 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Este es el link de la carpeta media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'AdopcionMascota/media/'))
+
