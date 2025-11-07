df05488ad3b63524090569865656992d82738bd0
alejoiza783 <aiza4840@gmail.com>
2025-11-06 21:36:38 -0500

    Add home route to fix 404 error

--- AdopcionMascota/urls.py
+++ AdopcionMascota/urls.py
@@ -1,40 +1,26 @@
-"""
-URL configuration for AdopcionMascota project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def test_view(request):
     return HttpResponse("✅ ¡La app funciona! El problema está en los templates o static files")
 
+def home(request):
+    return redirect('/auth/login/')  # Redirige al login automáticamente
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),  # ← AGREGAR ESTA LÍNEA (ruta principal)
     path('test/', test_view),
-     path('auth/', include('Aplicaciones.autenticacion.urls')),
+    path('auth/', include('Aplicaciones.autenticacion.urls')),
     path('mascotas/', include('Aplicaciones.Mascotas.urls')),
     path('personas/', include('Aplicaciones.Personas.urls')),
     path('adopciones/', include('Aplicaciones.Adopcion.urls')),
-
 ]
-
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 if settings.DEBUG:
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
